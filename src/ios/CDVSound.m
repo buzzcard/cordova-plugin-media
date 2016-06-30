5012088c8462a433fece0d65bb947c5fa33a14a1
Ryan Williams <ryan@ryanwilliams.id.au>
2016-06-30 16:26:04 +1000

    Update CDVSound.m

--- src/ios/CDVSound.m
+++ src/ios/CDVSound.m
@@ -649,9 +649,9 @@
         // jsString = [NSString stringWithFormat: @"%@(\"%@\",%d,%d);", @"cordova.require('cordova-plugin-media.Media').onStatus", mediaId, MEDIA_ERROR, MEDIA_ERR_DECODE];
         jsString = [NSString stringWithFormat:@"%@(\"%@\",%d,%@);", @"cordova.require('cordova-plugin-media.Media').onStatus", mediaId, MEDIA_ERROR, [self createMediaErrorWithCode:MEDIA_ERR_DECODE message:nil]];
     }
-    if (self.avSession) {
-        [self.avSession setActive:NO error:nil];
-    }
+    //if (self.avSession) {
+    //    [self.avSession setActive:NO error:nil];
+    //}
     [self.commandDelegate evalJs:jsString];
 }
 
